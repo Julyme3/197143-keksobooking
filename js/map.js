0d899c73f0628ce33c1dae50b6f21f6c0291ff98
JuliaGord <jg0311@bk.ru>
2017-05-26 19:37:35 +0300

    Генерация объявлений

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var TYPE = ['flat', 'house', 'bungalo'];
+var TYPE = [{en: 'flat', ru: 'Квартира'}, {en: 'house', ru: 'Дом'}, {en: 'bungalo', ru: 'Бунгало'}];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -21,48 +21,33 @@ var maxY = 500;
 var heightOfPin = 75;
 var widthOfPin = 56;
 var randomFeatures = [];
-var classFeature = [];
-var blockFeatures= [];
-var newSpan;
 
 // Функция для поиска случайного числа
 var getRandomLimit = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 };
-// Переименовываем элементы в массиве типа жилья
-var valueOfType = TYPE[getRandomLimit(0, TYPE.length - 1)];
-if (valueOfType === 'house') {
-  valueOfType = 'Дом';
-} else if (valueOfType === 'bungalo') {
-  valueOfType = 'Бунгало';
-} else {
-  valueOfType = 'Квартира';
+
+// Ищем случайное значение ключа ru в массиве объектов TYPE
+var valueOfType = TYPE[getRandomLimit(0, TYPE.length - 1)].ru;
+
+// Рандомно удаляем эелементы из массива FEATURES методом filter
+var getFeatures = function (array) {
+  randomFeatures = array.filter(function () { // принимаем массив
+    return (Math.random() * 10) & 1 ? true : false; // возвращаем правду если условие выполняется
+  });
+  return randomFeatures; // полученный массив из элементов возвращенных
+};
+getFeatures(FEATURES);
+
+// Функция для создания строки со span с классом features методом forEach
+var renderString = function (array) {
+  var feature = '';
+  array.forEach(function (item) {
+    feature += '<span class="feature__image feature__image--' + item + '"></span>';
+  });
+  return feature;
 };
 
-// Рандомно удаляем эелементы из массива FEATURES
-var getFeatures = function(array) {
-  randomFeatures = array.splice(getRandomLimit(0, array.length), getRandomLimit(0, array.length));
-  return randomFeatures;
-};
-
-// Создаем <span> по классам features
-var createFeatures = function () {
-  var result = getFeatures(FEATURES);
-  for (var i = 0; i <= result.length - 1; i++) {
-//    debugger;
-    classFeature [i] = 'feature__image feature__image--' + result [i];
-    blockFeatures [i] = document.querySelector('.lodge__features');
-    blockFeatures[i].insertAdjacentHTML('afterbegin', '<span class=' + classFeature[i] + '><span>');
-//    console.log(classFeature[i]);
-//    newSpan = document.createElement('span');
-//    blockFeatures = document.querySelector('.lodge__features');
-//    newSpan.className = classFeature [i];
-//    blockFeatures.appendChild(newSpan);
-  };
-//  return classFeature;
-};
-//createFeatures();
-//console.log(classFeature);
 /* Функция для вычисления уникального случайного числа по макс. длине массива */
 var numberRandomUniq; // случайное значение
 var arr = []; // массив для заполнения вычисленными случайными значениями
@@ -129,9 +114,8 @@ var createNotices = function (value) {
     };
 
   }
-  return notices [i];
+  return notices;
 };
-
 createNotices(quantityOfNotices);
 
 var blockMap = document.querySelector('.tokyo__pin-map');
@@ -141,8 +125,8 @@ for (var i = 0; i <= quantityOfNotices; i++) {
   var newNotice = document.createElement('div');
   var imgNotice = document.createElement('img');
   newNotice.className = 'pin';
-  newNotice.style.left = (getRandomLimit(minX, maxX) + (widthOfPin / 2)) + 'px';
-  newNotice.style.top = (getRandomLimit(minY, maxY) + heightOfPin) + 'px';
+  newNotice.style.left = (getRandomLimit(minX, maxX) - (widthOfPin / 2)) + 'px';
+  newNotice.style.top = (getRandomLimit(minY, maxY) - heightOfPin) + 'px';
   imgNotice.className = 'rounded';
   imgNotice.src = 'img/avatars/user' + 0 + authorAvatars[i] + '.png';
   imgNotice.style.width = '40px';
@@ -156,17 +140,23 @@ blockMap.appendChild(fragment);
 var similarNoticeTemplate = document.querySelector('#lodge-template');
 var similarListNotice = document.querySelector('#offer-dialog');
 var dialogPanel = document.querySelector('.dialog__panel');
+var ImgAuthor = document.querySelector('.dialog__title');
+var renderDialog = function (notice) {
 
-similarListNotice.removeChild(dialogPanel);
-var noticeElement = similarNoticeTemplate.content.cloneNode(true);
-noticeElement.querySelector('.lodge__title').textContent = TITLE[titleOfNotices[0]];
-noticeElement.querySelector('.lodge__address').textContent = notices[0].offer.address;
-noticeElement.querySelector('.lodge__price').innerHTML = notices[0].offer.price + '&#x20bd;/ночь';
-noticeElement.querySelector('.lodge__type').textContent = notices[0].offer.type;
-noticeElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + notices[0].offer.guests + ' гостей в ' + notices[0].offer.rooms + ' комнатах';
-noticeElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + notices[0].offer.checkin + ' , выезд до ' + notices[0].offer.checkout;
-//noticeElement.querySelector('.lodge__features').textContent = notices[0].offer.features;
-//for (var i = 0; i <= classFeature.length; i++){
-//noticeElement.querySelector('.lodge__features').innerHTML = '<span class=' + createFeatures() + '></span>';
-//}
-similarListNotice.appendChild(noticeElement);
+  similarListNotice.removeChild(dialogPanel);
+
+  var noticeElement = similarNoticeTemplate.content.cloneNode(true);
+
+  noticeElement.querySelector('.lodge__title').textContent = notice.offer.title;
+  noticeElement.querySelector('.lodge__address').textContent = notice.offer.address;
+  noticeElement.querySelector('.lodge__price').innerHTML = notice.offer.price + '&#x20bd;/ночь';
+  noticeElement.querySelector('.lodge__type').textContent = notice.offer.type;
+  noticeElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + notices[0].offer.guests + ' гостей в ' + notice.offer.rooms + ' комнатах';
+  noticeElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + notices[0].offer.checkin + ' , выезд до ' + notice.offer.checkout;
+  noticeElement.querySelector('.lodge__features').innerHTML = renderString(notice.offer.features);
+  noticeElement.querySelector('.lodge__description').textContent = notice.offer.description;
+  ImgAuthor.firstElementChild.src = notice.author.avatar;
+
+  similarListNotice.appendChild(noticeElement);
+};
+renderDialog(notices[0]);
