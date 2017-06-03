cee30300c45eeecc10947678a91b437da2491f15
JuliaGord <jg0311@bk.ru>
2017-06-03 23:37:45 +0300

    Добавлены обработчики событий для pin/модального окна

--- js/map.js
+++ js/map.js
@@ -160,3 +160,82 @@ var renderDialog = function (notice) {
   similarListNotice.appendChild(noticeElement);
 };
 renderDialog(notices[0]);
+
+// Переменные для событий
+
+var pinMap = document.querySelector('.tokyo__pin-map')
+var pins = pinMap.querySelectorAll('.pin');
+var dialogBlock = document.querySelector('.dialog');
+var dialogClose = dialogBlock.querySelector('.dialog__close');
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
+
+var popupEscHandlier = function (evt) {
+  if (evt.keyCode === ESC_KEY_CODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  dialogBlock.classList.remove('hidden');
+  pin.classList.add('pin--active');
+
+// закрываем попап нажатием Esc на документе
+  document.addEventListener('keydown', popupEscHandlier);
+
+};
+
+var closePopup = function () {
+  dialogBlock.classList.add('hidden');
+  for (i = 0; i < pins.length; i++) {
+    var pin = pins[i];
+    pin.classList.remove('pin--active');
+  }
+
+// убираем слушатель Esc на документе
+  document.removeEventListener('keydown', popupEscHandlier);
+};
+
+// при нажатии на пин открываем объявление и добавляем класс active пину, если уже есть класс active - удаляется (в цикле ищем...)
+pinMap.addEventListener('click', function (evt) {
+  var target = evt.target;
+
+  // если уже есть класс active - удаляем
+  for (i = 0; i < pins.length; i++) {
+    var pin = pins[i];
+    if (pin.classList.contains('pin--active')) {
+      pin.classList.remove('pin--active');
+    }
+  }
+
+  if (target.classList.contains('pin')) {
+    target.classList.add('pin--active');
+  }
+
+  dialogBlock.classList.remove('hidden');
+});
+
+// всем пинам добавляем tabindex
+for (i = 0; i < pins.length; i++) {
+  var pin = pins[i];
+  pin.setAttribute('tabindex', 0);
+}
+
+// открываем попап нажатием Enter на пин
+pinMap.addEventListener('keydown', function (evt) {
+  if (evt.target.classList.contains('pin') && evt.keyCode === ENTER_KEY_CODE) {
+    openPopup();
+  }
+});
+
+// при нажатии на крест объявление закрывается, класс active у пина удаляется (ищется в цикле)
+dialogClose.addEventListener('click', function () {
+  closePopup();
+});
+
+// закрываем попап нажатием Enter на крест
+dialogClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY_CODE) {
+    closePopup();
+  }
+});
